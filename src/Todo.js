53b63ca1b4136309b0155efda45f124983cdebf5
hicmoh <79941068+hicmoh@users.noreply.github.com>
2022-03-08 18:20:32 -0500

    Add files via upload

---
+++ src/Todo.js
@@ -0,0 +1,146 @@
+import React,{Component } from 'react';
+
+
+class Todo extends Component {
+
+	 constructor(props){
+      super(props);
+
+      
+
+	this.state={
+      edit:true,
+      id:null,
+      mockData: [{
+
+      	id:'1',
+      	title:'LAPTOP',
+      	done:false,
+      	date: new Date()
+      },{
+        id:'2',
+      	title:'Monitor',
+      	done:false,
+      	date: new Date()
+
+
+      },
+      {
+        id:'3',
+      	title:'Desktop',
+      	done:false,
+      	date: new Date()
+
+
+      },
+      {
+        id:'4',
+      	title:'Router',
+      	done:false,
+      	date: new Date()
+
+         }]
+
+        }
+	}
+   onSubmitHandle(event) {
+    event.preventDefault();
+
+    this.setState({
+      mockData: [...this.state.mockData, {
+        id: Date.now(),
+        title: event.target.item.value,
+        done: false,
+        date: new Date()
+      }]
+    });
+
+    event.target.item.value = '';
+  }
+    onDeleteHandle() {
+    let id = arguments[0];
+
+    this.setState({
+      mockData: this.state.mockData.filter(item => {
+        if (item.id !== id) {
+          return item;
+        }
+      })
+    });
+  }
+   onEditHandle(event) {
+    this.setState({
+      edit: true,
+      id: arguments[0],
+      title: arguments[1]
+    });
+  }
+
+onUpdateHandle(event) {
+    event.preventDefault();
+
+    this.setState({
+      mockData: this.state.mockData.map(item => {
+        if (item.id === this.state.id) {
+          item['title'] = event.target.updatedItem.value;
+          return item;
+        }
+
+        return item;
+      })
+    });
+
+    this.setState({
+      edit: false
+    });
+  }
+
+   onCompleteHandle() {
+    let id = arguments[0];
+
+    this.setState({
+      mockData: this.state.mockData.map(item => {
+        if (item.id === id) {
+          item['done'] = true;
+          return item;
+        }
+
+        return item;
+      })
+    });
+  }
+
+	
+ renderEditForm() {
+    if (this.state.edit) {
+      return <form onSubmit={this.onUpdateHandle.bind(this)}>
+        <input type="text" name="updatedItem" className="item" defaultValue={this.state.title} />
+        <button className="update-add">Update</button>
+      </form>
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderEditForm()}
+        <form onSubmit={this.onSubmitHandle.bind(this)}>
+          <input type="text" name="item" className="item" />
+          <button className="btn-add-item">Add</button>
+        </form>
+        <ul>
+          {this.state.mockData.map(item => (
+            <li key={item.id} className={ item.done ? 'done' : 'hidden' }>
+              {item.title}
+              <button onClick={this.onDeleteHandle.bind(this, item.id)}>Delete</button>
+              <button onClick={this.onEditHandle.bind(this, item.id, item.title)}>Edit</button>
+              <button onClick={this.onCompleteHandle.bind(this, item.id)}>Complete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Todo;
