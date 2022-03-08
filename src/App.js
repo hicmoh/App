bce0604303510335b431a60945d8524dd07eadcf
hicmoh <79941068+hicmoh@users.noreply.github.com>
2022-03-08 18:18:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, {Component}from 'react'
+import logo from './logo.svg'
+import'./App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+//import Todo component here 
+import Todo from'./Todo';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
 
-export default App
+  constructor(props){
+      super(props);
+
+      this.state={
+        show:false
+      };
+    }
+
+    render(){
+
+    //use Todo component inside render method.
+
+         return (
+
+         
+          <div className="App">
+
+               <Todo/>
+          </div>
+          
+
+
+          );
+      }
+  }
+
+
+export default App;
